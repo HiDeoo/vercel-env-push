37383b42bfbe359972647be6051e5e4df5098836
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-07-04 18:18:54 +0200

    Harden env var push test suite

--- test/vercel.test.ts
+++ test/vercel.test.ts
@@ -40,16 +40,22 @@ describe('env var', () => {
     vi.clearAllMocks()
   })
 
-  test('should push a basic variable to a single environment', async () => {
+  test('should push environment variables to a single environment', async () => {
     const env = 'production'
 
-    await pushEnvVars('test/fixtures/.env.basic', [env])
+    await pushEnvVars('test/fixtures/.env.test', [env])
 
     const spy = vi.mocked(execa)
 
-    expect(spy).toHaveBeenCalledTimes(2)
+    expect(spy).toHaveBeenCalledTimes(6)
 
     expect(spy).toHaveBeenNthRemoveEnvCall(1, env, 'keyA')
     expect(spy).toHaveBeenNthAddEnvCall(2, env, 'keyA', 'valueA')
+
+    expect(spy).toHaveBeenNthRemoveEnvCall(3, env, 'keyAExpanded')
+    expect(spy).toHaveBeenNthAddEnvCall(4, env, 'keyAExpanded', 'valueA')
+
+    expect(spy).toHaveBeenNthRemoveEnvCall(5, env, 'keyB')
+    expect(spy).toHaveBeenNthAddEnvCall(6, env, 'keyB', 'valueB')
   })
 })
